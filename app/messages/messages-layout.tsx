ab6d9edd487e4d5866e26f0b5411dd9c66d3423a
Max Ngew <maxngewjs@gmail.com>
2026-04-26 22:51:33 +0800

    fix: comment out unused Search and Link imports

--- app/messages/messages-layout.tsx
+++ app/messages/messages-layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-//import { MessageSquare, Send, Search } from "lucide-react";
+import { MessageSquare, Send /*Search*/ } from "lucide-react";
 //import Link from "next/link";
 import { useState } from "react";
 
